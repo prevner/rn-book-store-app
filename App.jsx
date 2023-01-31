73b96b0d17795a348e54c79111d39e0ce1c94bb2
Dixy Prevner M'POUTOU BABINGUI <dixy-prevner.m-poutou-babingui@estiam.com>
2023-01-31 10:09:25 +0100

    lastCommit

--- App.jsx
+++ App.jsx
@@ -1,17 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, LogBox, Text, View } from 'react-native';
 import { profile,books, myBooks,categories } from './data';
-import { myboo } from "./screens/";
 // Hide Error FlatList using inside ScrollView
 LogBox.ignoreLogs(['VirtualizedLists']);
 
-/* const theme = {
-  ...DefaultTheme,
-  colors: {
-      ...DefaultTheme.colors,
-      border: "transparent"
-  }
-} */
+
 
 import HomeScreen from './screens/HomeScreen';
 
